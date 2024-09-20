bddda7017b518883cdc649b3ebeb94f36cf5aa03
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-09-20 17:22:16 +0530

    Added routes

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,21 @@
+import  {NextRequest, NextResponse} from 'next/server'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import {connect} from '@/dbConfig/dbConfig'
+
+connect()
+
+export async function POST(request: NextRequest) {
+    try {
+        const response=NextResponse.json({message: 'Logout successful', success: true})
+
+        response.cookies.set("token","",{
+            httpOnly: true,
+            expires: new Date(0),
+        })
+
+         return response
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,22 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(req:NextRequest){
+
+    try {
+        const userId = await getDataFromToken(req);
+        const user = await User.findOne({_id: userId}).select("-password");
+        return NextResponse.json({
+            mesaaage: "User found",
+            data: user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 400});
+    }
+
+}
